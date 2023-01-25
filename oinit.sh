718255195213eba538894c667e356266e4efd4eb
Rezec <RezecNoble@gmail.com>
2023-01-24 22:04:17 -0500

    Additional Modifications

--- oinit.sh
+++ oinit.sh
@@ -1,54 +1,84 @@
 #!/bin/bash
 
-echo "Initializing Server and Checking for updates"
+#Pause function
+function pause()
+{
+        printf -- "\n\n"
+        read -s -n 1 -p "Press any button to continue setup or ctrl+c to quit..."
+        printf -- "\n\n"
+}
+
+printf --  "\n####################\n\nWelcome to the SWG Server Preparation Script!\nThis script is designed primarily for Oracle Linux 8.\nThis may not work on distros without those available packages.\n\n####################\n"
+
+pause
+
+printf --  "\nInitializing Server and Checking for updates\n"
 #Update
 sudo yum check-update
 
+pause
+
 #Install Oracle Preinstall package
-echo "Running Oracle Preinstall Package"
+printf --  "\nRunning Oracle Preinstall Package\n"
 sudo yum install -y oracle-database-preinstall-19c.x86_64
 #Future Use/Testing for 64-bit
 #sudo yum install -y oracle-database-preinstall-21c.x86_64
 
-echo "Setting SELinux to permissive"
+pause
+
+printf --  "\nSetting SELinux to permissive\n"
 #Set SELinux to Permissive
 sudo setenforce Permissive
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 
-echo "Making a folder for dependencies"
+pause
+
+printf --  "\nMaking a folder for dependencies\n"
 #Make a folder for dependencies
 mkdir ~/dependencies
 cd ~/dependencies
 
-echo "Downloading and installing Azul Java 11"
+printf --  "\nDownloading and installing Azul Java 11\n"
 #Download and install java (be sure to check Azul for the latest 32-bit Java 11 for SWG and 64-bit for sqldeveloper)
 wget https://cdn.azul.com/zulu/bin/zulu11.62.17-ca-jdk11.0.18-linux.i686.rpm
 sudo yum install ./zulu11.62.17-ca-jdk11.0.18-linux.i686.rpm -y
 wget https://cdn.azul.com/zulu/bin/zulu11.62.17-ca-jdk11.0.18-linux.x86_64.rpm
 sudo yum install ./zulu11.62.17-ca-jdk11.0.18-linux.x86_64.rpm -y
 
-echo "Installing additional dependencies for 32bit"
+pause
+
+printf --  "\nInstalling additional dependencies for 32bit\n"
 sudo yum install libXext.i686 libXrender.i686 libXtst.i686 -y
 
-echo "Installing Python and Pip"
+pause
+
+printf --  "\nInstalling Python and Pip\n"
 #Install Python (This command could vary by distro)
 sudo yum install python39 -y
 sudo yum install python39-pip -y
 sudo yum install python39-ply -y
 
-echo "Que Set PATHs"
+pause
+
+printf --  "\nQue Set PATHs\n"
 #Set Paths in Oracle bashrc
 sudo sh /home/swg/swg-prepare/oracle_paths.sh
 
+pause
+
 #Download Oracle 19.3.0 Database
-echo "Queing Oracle Database for download"
+printf --  "\nQueing Oracle Database for download\n"
 sh /home/swg/swg-prepare/oracle_downloads.sh
 
+pause
+
 #Make directories and extract
-echo "Que Oracle DB Extract"
+printf --  "\nQue Oracle DB Extract\n"
 sudo sh /home/swg/swg-prepare/oracle_extract.sh
 
+pause
+
 #Move install scripts to Oracle User
-echo "Que install scripts for transfer to Oracle user"
+printf --  "\nQue install scripts for transfer to Oracle user\n"
 sudo sh /home/swg/swg-prepare/oracle_user.sh
--- swginit.sh
+++ swginit.sh
@@ -1,37 +1,54 @@
 #!/bin/bash
 
+#Pause function
+function pause()
+{
+        printf -- "\n\n"
+        read -s -n 1 -p "Press any button to continue setup or ctrl+c to quit..."
+        printf -- "\n\n"
+}
+
 #get some dependencies
-echo "Installing Dependencies for Compiling SWG"
+printf --  "\nInstalling Dependencies for Compiling SWG\n"
 yum install ant clang cmake libaio.i686 libgcc.i686 glibc.i686 glibc-devel.i686 libstdc++-devel.i686 ncurses-devel.i686 libxml2-devel libxml2-devel.i686 pcre-devel pcre-devel.i686 libcurl.i686 libcurl-devel libcurl-devel.i686 boost boost.i686 boost-devel boost-devel.i686 sqlite-devel sqlite-devel.i686 libnsl.i686 -y
 
+pause
+
 #TODO: Rewrite those build scripts in Python3 as Python2 is Depracted
-echo "Installing Python2"
+printf --  "\nInstalling Python2\n"
 #Install Python 2 to compile CRCs
 yum install python2 -y
 cp /usr/bin/python2 /usr/bin/python
 
+pause
+
 #Install oracle instantclients
-echo "Installing Oracle Instantclients"
+printf --  "\nInstalling Oracle Instantclients\n"
 rpm -Uhv /home/swg/dependencies/oracle-instantclient18.3-basic-18.3.0.0.0-1.i386.rpm
 rpm -Uhv /home/swg/dependencies/oracle-instantclient18.3-devel-18.3.0.0.0-1.i386.rpm
 rpm -Uhv /home/swg/dependencies/oracle-instantclient18.3-sqlplus-18.3.0.0.0-1.i386.rpm
 
+pause
+
 #Install SQLDeveloper
-echo "Installing SQLDeveloper"
+printf --  "\nInstalling SQLDeveloper\n"
 cd ~/dependencies
 #rpm -Uhv /home/swg/dependencies/sqldeveloper-20.2.0.175.1842-20.2.0-175.1842.noarch.rpm
 rpm -Uhv /home/swg/dependencies/sqldeveloper-22.2.1.234.1810.noarch.rpm
 #Set SQLDeveloper to use 64-bit java
-echo "SetJavaHome  /usr/lib/jvm/zulu-11" | tee -a /opt/sqldeveloper/sqldeveloper/bin/sqldeveloper.conf
+printf --  "SetJavaHome  /usr/lib/jvm/zulu-11" | tee -a /opt/sqldeveloper/sqldeveloper/bin/sqldeveloper.conf
+
+pause
 
 #set env vars
+printf --  "\nSetting Environment Variables\n"
 touch /etc/ld.so.conf.d/oracle.conf
 touch /etc/profile.d/oracle.sh
-echo "/usr/lib/oracle/18.3/client/lib" | tee -a /etc/ld.so.conf.d/oracle.conf
-echo "export ORACLE_HOME=/usr/lib/oracle/18.3/client" | tee -a /etc/profile.d/oracle.sh
-echo "export PATH=$PATH:/usr/lib/oracle/18.3/client/bin" | tee -a /etc/profile.d/oracle.sh
-echo "export LD_LIBRARY_PATH=/usr/lib/oracle/18.3/client/lib:/usr/include/oracle/18.3/client" | tee -a /etc/profile.d/oracle.sh
+printf --  "/usr/lib/oracle/18.3/client/lib" | tee -a /etc/ld.so.conf.d/oracle.conf
+printf --  "export ORACLE_HOME=/usr/lib/oracle/18.3/client" | tee -a /etc/profile.d/oracle.sh
+printf --  "export PATH=$PATH:/usr/lib/oracle/18.3/client/bin" | tee -a /etc/profile.d/oracle.sh
+printf --  "export LD_LIBRARY_PATH=/usr/lib/oracle/18.3/client/lib:/usr/include/oracle/18.3/client" | tee -a /etc/profile.d/oracle.sh
 ln -sf /usr/include/oracle/18.3/client $ORACLE_HOME/include
 ldconfig
 touch /etc/profile.d/java.sh
-echo "export JAVA_HOME=/usr/lib/jvm/zulu-11-x86" | tee -a /etc/profile.d/java.sh
+printf --  "export JAVA_HOME=/usr/lib/jvm/zulu-11-x86" | tee -a /etc/profile.d/java.sh
